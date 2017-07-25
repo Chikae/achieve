6e61dafb008f361292f8bba377e799bab5eec7be
柳田史瑛 <chikae@yanagidafumiakiranoMacBook-Pro.local>
2017-07-25 21:43:52 +0900

    xxxx

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,8 +81,8 @@ config.action_mailer.default_url_options = { host: 'https://bref-bastille-96619.
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings =
   {
-   user_name: ENV["app72809195@heroku.com"],
-   password: ENV["jvorvs7m7116"],
+   user_name: ENV["SENDGRID_USERNAME"],
+   password: ENV["SENDGRID_PASSWORD"],
    domain: "heroku.com",
    address: "smtp.sendgrid.net",
    port: 587,
